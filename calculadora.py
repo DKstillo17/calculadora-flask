e13a1a1d03db77cf53a1af5be91c1ec482f8cb1d
DKstill17 <dkstillo94@gmail.com>
2021-05-13 21:36:40 -0500

    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/calculadora', methods=['POST'])
+def send(total=sum):
+    if request.method == 'POST':
+        
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operacion = request.form['operacion']
+
+        if operacion == 'suma':
+            total = float(num1) + float(num2)
+            return render_template('index.html', total=total)
+
+        elif operacion == 'resta':
+            total = float(num1) - float(num2)
+            return render_template('index.html', total=total)
+
+        elif operacion == 'multiplicacion':
+            total = float(num1) * float(num2)
+            return render_template('index.html', total=total)
+
+        elif operacion == 'division':
+            total = float(num1) / float(num2)
+            return render_template('index.html', total=total)
+        else:
+            return render_template('index.html')
+
+
+if __name__ == ' __main__':
+    app.debug = True
+    app.run()
+
